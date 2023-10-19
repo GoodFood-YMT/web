4f04fa9b26a8f0765dfc3baea80e955547077271
tom-brulin <t.brulin@outlook.com>
2023-10-19 11:51:13 +0200

    feat: show products of a restaurant

--- src/components/products/in_stock_products_by_restaurant.tsx
+++ src/components/products/in_stock_products_by_restaurant.tsx
@@ -1,47 +1,68 @@
 "use client";
 
 import Link from "next/link";
+import { Euro } from "lucide-react";
 import { AiOutlineLoading } from "react-icons/ai";
 import { useFetchInStockProductsByRestaurant } from "~/hooks/catalog/products/use_fetch_products_by_restaurant";
 import { cn } from "~/utils/cn";
 
 interface Props {
-    restaurantId: string;
-};
-
-export const InStockProductsByRestaurant = ({restaurantId}: Props) => {
-    console.log(restaurantId)
-    const products = useFetchInStockProductsByRestaurant(restaurantId);
-
-    if(products.isError){
-        return <div>Something went wrong</div>
-    }
-
-    return (
-        <>
-            <div className={cn("grid grid-cols-4")}>
-                {products.data?.pages.map((page) =>
-                        page.data.map((product) => (
-                            <Link href={`/restaurants/${restaurantId}/products/${product.id}`} key={product.id}>
-                                {product.label}
-                            </Link>
-                        ))
-                    )
-                }
-
-                {products.isLoading && (
-                    <AiOutlineLoading className={cn("h-6 w-6 animate-spin")} />
-                )}
-
-                {products.hasNextPage && (
-                    <button
-                        onClick={() => products.fetchNextPage()}
-                        disabled={products.isLoading}
-                    >
-                        Load more
-                    </button>
-                )}
-            </div>
-        </>
-    )
+  restaurantId: string;
 }
+
+export const InStockProductsByRestaurant = ({ restaurantId }: Props) => {
+  const products = useFetchInStockProductsByRestaurant(restaurantId);
+
+  if (products.isError) {
+    return <div>Something went wrong</div>;
+  }
+
+  return (
+    <>
+      <div className={cn("grid grid-cols-4 gap-4")}>
+        {products.data?.pages.map((page) =>
+          page.data.map((product) => (
+            <Link
+              href={`/restaurants/${restaurantId}/products/${product.id}`}
+              key={product.id}
+            >
+              <div className="h-[250px] w-full rounded-md bg-[url('https://images.unsplash.com/photo-1565299507177-b0ac66763828?auto=format&fit=crop&q=80&w=2522&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center">
+                <div className="relative h-full w-full rounded-md bg-black/30">
+                  {product.quantity > 0 ? (
+                    <span className="absolute left-2 top-2 rounded-full bg-green-300 px-2 text-xs text-green-900">
+                      In stock
+                    </span>
+                  ) : (
+                    <span className="absolute left-2 top-2 rounded-full bg-red-300 px-2 text-xs text-red-900">
+                      Out of stock
+                    </span>
+                  )}
+                  <span className="absolute right-2 top-2 flex items-center rounded-full bg-white px-2 py-1 text-xs text-black">
+                    {product.price}
+                    <Euro size={12} />
+                  </span>
+                  <span className="absolute bottom-2 left-2 text-lg font-medium text-white">
+                    {product.label}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          )),
+        )}
+
+        {products.isLoading && (
+          <AiOutlineLoading className={cn("h-6 w-6 animate-spin")} />
+        )}
+
+        {products.hasNextPage && (
+          <button
+            onClick={() => products.fetchNextPage()}
+            disabled={products.isLoading}
+          >
+            Load more
+          </button>
+        )}
+      </div>
+    </>
+  );
+};
