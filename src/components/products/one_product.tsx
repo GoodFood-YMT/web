a5228c862ff68b6f7c80a96486d10edbc811eb10
tom-brulin <t.brulin@outlook.com>
2023-10-19 12:01:13 +0200

    feat: product page

--- src/components/products/one_product.tsx
+++ src/components/products/one_product.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "~/components/ui/button";
 import { useFetchProductById } from "~/hooks/catalog/products/use_fetch_product_by_id";
 
 interface Props {
@@ -18,11 +19,17 @@ export const OneProduct = ({ id }: Props) => {
   }
 
   return (
-    <div>
-      <p>Product: {product.data.label}</p>
-      <p>Description: {product.data.description}</p>
-      <p>Price: {product.data.price} €</p>
-      <p>Quantity: {product.data.quantity}</p>
+    <div className="grid grid-cols-2 gap-8">
+      <div className="h-[500px] w-full rounded-md bg-[url('https://images.unsplash.com/photo-1565299507177-b0ac66763828?auto=format&fit=crop&q=80&w=2522&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center"></div>
+      <div className="flex flex-col gap-4">
+        <span className="flex justify-between text-xl font-medium">
+          <span>{product.data.label}</span> <span>{product.data.price}€</span>
+        </span>
+        <p>{product.data.description}</p>
+        <Button disabled={product.data.quantity <= 0}>
+          {product.data.quantity > 0 ? "Add to cart" : "Not in stock"}
+        </Button>
+      </div>
     </div>
   );
 };
