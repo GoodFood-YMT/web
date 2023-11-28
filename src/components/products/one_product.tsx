d336e35c46e785efc882205b12e00cb42eaf15eb
tom-brulin <t.brulin@outlook.com>
2023-11-28 14:24:35 +0100

    feat: product page

--- src/components/products/one_product.tsx
+++ src/components/products/one_product.tsx
@@ -1,11 +1,12 @@
 "use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import { ShoppingBag } from "lucide-react";
 import toast from "react-hot-toast";
 import { Button } from "~/components/ui/button";
-import { Input } from "~/components/ui/input";
 import { useAddToBasket } from "~/hooks/basket/use_add_to_basket";
 import { useFetchProductById } from "~/hooks/catalog/products/use_fetch_product_by_id";
+import { getRestaurantImage } from "~/utils/get_restaurant_image";
 
 interface Props {
   id: string;
@@ -14,10 +15,9 @@ interface Props {
 export const OneProduct = ({ id }: Props) => {
   const product = useFetchProductById(id);
   const addToBasket = useAddToBasket();
-  const [quantity, setQuantity] = useState(1);
 
   const onAddToBasket = () => {
-    addToBasket.mutate({ id, quantity });
+    addToBasket.mutate({ id, quantity: 1 });
     toast.success("Product added to basket");
   };
 
@@ -30,22 +30,30 @@ export const OneProduct = ({ id }: Props) => {
   }
 
   return (
-    <div className="grid grid-cols-2 gap-8">
-      <div className="bg-product h-[500px] w-full rounded-md bg-cover bg-center"></div>
-      <div className="flex flex-col gap-4">
-        <span className="flex justify-between text-xl font-medium">
-          <span>{product.data.label}</span> <span>{product.data.price}€</span>
-        </span>
-        <p>{product.data.description}</p>
-        <div className="grid grid-cols-2 gap-4">
-          <Input
-            type="number"
-            value={quantity}
-            min={1}
-            onChange={(e) => setQuantity(parseInt(e.target.value))}
-          />
-          <Button disabled={product.data.quantity <= 0} onClick={onAddToBasket}>
-            {product.data.quantity > 0 ? "Add to cart" : "Not in stock"}
+    <div className="grid grid-cols-[1fr,2fr] gap-4 bg-white p-4 shadow-sm">
+      <div className="relative aspect-square w-full">
+        <Image
+          src={getRestaurantImage(product.data.label)}
+          alt="Product"
+          className="object-cover object-center"
+          fill
+        />
+      </div>
+      <div className="flex flex-col justify-between">
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between">
+            <h2 className="text-lg font-medium tracking-tight">
+              {product.data.label}
+            </h2>
+            <span className="text-lg font-medium tracking-tight">
+              {product.data.price} €
+            </span>
+          </div>
+          <p className="text-sm opacity-60">{product.data.description}</p>
+        </div>
+        <div className="flex justify-end">
+          <Button onClick={onAddToBasket}>
+            <ShoppingBag size={16} /> Add to basket
           </Button>
         </div>
       </div>
