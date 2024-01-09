cff218065f5256b9d49ba7bcb93917a29a7f5038
MatthieuGallice <frooztor@gmail.com>
2024-01-09 16:03:39 +0100

    Component Provider: add button

---
+++ src/components/providers/add_provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export const AddProvider = () => {
+
+    return (
+        <div>
+            <a href="">
+                <button>New</button>
+            </a>
+        </div>
+    );
+};
