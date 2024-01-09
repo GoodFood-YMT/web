ffdeb3a1695dac5656af5a82cabc76bcfc3d3f9a
tom-brulin <t.brulin@outlook.com>
2024-01-09 10:07:57 +0100

    feat: full checkout system

--- src/components/basket/checkout.tsx
+++ src/components/basket/checkout.tsx
@@ -1,12 +1,83 @@
 "use client";
 
 import { useState } from "react";
-import { CheckIcon, CreditCardIcon, InfoIcon } from "lucide-react";
-import { Button } from "~/components/ui/button";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { CheckIcon, CreditCardIcon, Divide, InfoIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { z } from "zod";
+import { Button, buttonVariants } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { useClearBasket } from "~/hooks/basket/use_clear_basket";
+import { useFetchBasket } from "~/hooks/basket/use_fetch_basket";
+import { useFetchAllAddresses } from "~/hooks/delivery/use_fetch_addresses";
+import { useCreateOrder } from "~/hooks/ordering/use_create_order";
 import { cn } from "~/utils/cn";
 
+const formSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  addressId: z.string(),
+  creditCardNumber: z.string(),
+  creditCardExpiry: z.string(),
+  creditCardCvv: z.string(),
+});
+
 export const Checkout = () => {
+  const router = useRouter();
   const [step, setStep] = useState<1 | 2>(1);
+
+  const addresses = useFetchAllAddresses();
+  const basket = useFetchBasket();
+  const createOrder = useCreateOrder();
+  const clearBasket = useClearBasket();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const handleSubmit = (payload: z.infer<typeof formSchema>) => {
+    if (payload.addressId && basket.data) {
+      createOrder.mutate(
+        {
+          addressId: payload.addressId,
+          products: basket.data.items.map((item) => ({
+            productId: item.id,
+            quantity: item.quantity,
+          })),
+        },
+        {
+          onSuccess: async () => {
+            await clearBasket.mutateAsync();
+            await basket.refetch();
+            toast.success("Order placed");
+            router.push("/account");
+          },
+          onError: () => {
+            toast.error("An error occurred");
+          },
+        },
+      );
+    }
+  };
+
+  if (basket.isLoading) {
+    return null;
+  }
+
+  if (!basket.data?.items.length || basket.data?.items.length === 0) {
+    router.push("/");
+    return null;
+  }
 
   return (
     <div>
@@ -31,50 +102,183 @@ export const Checkout = () => {
         </span>
       </div>
 
-      {step === 1 && (
-        <div className="bg-white p-4 shadow-sm">
-          <div className="flex h-full flex-col justify-between gap-4">
-            <div className="flex flex-col gap-2 overflow-y-auto">
-              <h2 className="mb-2 text-lg font-medium tracking-tight">
-                Information
-              </h2>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          {step === 1 && (
+            <div className="bg-white p-4 shadow-sm">
+              <div className="flex h-full flex-col justify-between gap-4">
+                <div className="flex flex-col gap-2 overflow-y-auto">
+                  <h2 className="mb-2 text-lg font-medium tracking-tight">
+                    Information
+                  </h2>
 
-              <div className="flex justify-end">
-                <Button
-                  onClick={() => {
-                    setStep(2);
-                  }}
-                >
-                  Next
-                </Button>
+                  <div className="flex w-full gap-4">
+                    <FormField
+                      control={form.control}
+                      name="firstName"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Firstname</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="lastName"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Lastname</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+
+                  <h2 className="mb-2 text-lg font-medium tracking-tight">
+                    Shipping Address
+                  </h2>
+
+                  {addresses.data?.addresses.map((address) => (
+                    <div
+                      key={address.id}
+                      className={cn(
+                        "cursor-pointer border p-4 shadow-sm hover:bg-gray-50",
+                        {
+                          "bg-gray-50": form.watch("addressId") === address.id,
+                        },
+                      )}
+                      onClick={() => {
+                        form.setValue("addressId", address.id);
+                      }}
+                    >
+                      <h3 className="mb-1 text-base font-medium tracking-tight">
+                        {address.name}
+                      </h3>
+
+                      <p className="text-sm">{address.street}</p>
+                      <p className="text-sm">
+                        {address.zip_code} {address.city}, {address.country}
+                      </p>
+                    </div>
+                  ))}
+
+                  {addresses.data?.addresses.length &&
+                    addresses.data?.addresses.length <= 0 && (
+                      <div className="border p-4 shadow-sm">
+                        <h3 className="mb-1 flex items-center justify-between text-base font-medium tracking-tight">
+                          No addresses
+                          <Link
+                            href="/account/addresses/add"
+                            className={buttonVariants({ size: "xs" })}
+                          >
+                            Add one
+                          </Link>
+                        </h3>
+                      </div>
+                    )}
+
+                  <div className="mt-2 flex justify-end">
+                    <Button
+                      onClick={() => {
+                        setStep(2);
+                      }}
+                      disabled={
+                        !form.watch("addressId") ||
+                        !form.watch("firstName") ||
+                        !form.watch("lastName")
+                      }
+                    >
+                      Next
+                    </Button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          )}
 
-      {step === 2 && (
-        <div className="bg-white p-4 shadow-sm">
-          <div className="flex h-full flex-col justify-between gap-4">
-            <div className="flex flex-col gap-2 overflow-y-auto">
-              <h2 className="mb-2 text-lg font-medium tracking-tight">
-                Payment
-              </h2>
+          {step === 2 && (
+            <div className="bg-white p-4 shadow-sm">
+              <div className="flex h-full flex-col justify-between gap-4">
+                <div className="flex flex-col gap-2 overflow-y-auto">
+                  <h2 className="mb-2 text-lg font-medium tracking-tight">
+                    Payment
+                  </h2>
 
-              <div className="flex justify-between">
-                <Button
-                  onClick={() => {
-                    setStep(1);
-                  }}
-                >
-                  Previous
-                </Button>
-                <Button>Proceed to payment</Button>
+                  <FormField
+                    control={form.control}
+                    name="creditCardNumber"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Credit Card Number</FormLabel>
+                        <FormControl>
+                          <Input {...field} />
+                        </FormControl>
+                      </FormItem>
+                    )}
+                  />
+
+                  <div className="flex w-full gap-4">
+                    <FormField
+                      control={form.control}
+                      name="creditCardCvv"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>CVV Code</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="creditCardExpiry"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Expiry Date</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+
+                  <div className="mt-2 flex justify-between">
+                    <Button
+                      onClick={() => {
+                        setStep(1);
+                      }}
+                    >
+                      Previous
+                    </Button>
+                    <Button
+                      type="submit"
+                      disabled={
+                        !form.watch("addressId") ||
+                        !form.watch("firstName") ||
+                        !form.watch("lastName") ||
+                        !form.watch("creditCardNumber") ||
+                        !form.watch("creditCardExpiry") ||
+                        !form.watch("creditCardCvv")
+                      }
+                    >
+                      Proceed to payment
+                    </Button>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      )}
+          )}
+        </form>
+      </Form>
     </div>
   );
 };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "~/utils/cn";
 
 const buttonVariants = cva(
-  "inline-flex gap-2 items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex gap-2 items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
