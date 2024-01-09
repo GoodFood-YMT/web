85b5c300dfd734d30f6b2a07a77f7d6689522d52
MatthieuGallice <frooztor@gmail.com>
2024-01-09 16:04:50 +0100

    Component all Provider: button ADD & design

--- src/components/providers/all_providers_home.tsx
+++ src/components/providers/all_providers_home.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useFetchAllProviders } from "~/hooks/providers/use_fetch_all_providers";
+import { AddProvider } from "~/components/providers/add_provider";
 
 export const AllProvidersHome = () => {
   const providers = useFetchAllProviders();
@@ -11,14 +12,34 @@ export const AllProvidersHome = () => {
 
   return (
     <>
-    {providers.data?.pages.map((page) =>
-        page.data.map((providers) => (
-          <link href={`/providers/${providers.id}/ingredients`} key={providers.id}>
-            {providers.name}
-          </link>
-        )),
-      )}
-
+    <h1>Providers</h1>
+    <AddProvider />
+    <table>
+      <thead>
+        <tr>
+          <th>Ingredients</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+        <tbody>
+          {providers.data?.pages.map((page) =>
+            page.data.map((providers) => (
+              <tr>
+                <td>
+                  <link>
+                    {providers.id}
+                  </link>
+                </td>
+                <td>
+                  <link href={`/providers/${providers.id}/ingredients`} key={providers.id}>
+                    {providers.name}
+                  </link>
+                </td>
+              </tr>
+            )),
+            )}
+        </tbody>
+      </table>
       {providers.hasNextPage && (
         <button
           onClick={() => providers.fetchNextPage()}
