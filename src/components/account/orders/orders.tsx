3df70d428fa372098d65dfef0845359cabd473f3
tom-brulin <t.brulin@outlook.com>
2024-01-09 11:37:31 +0100

    feat: add view all orders button

--- src/components/account/orders/orders.tsx
+++ src/components/account/orders/orders.tsx
@@ -1,17 +1,21 @@
 "use client";
 
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { DateTime } from "luxon";
+import { Button, buttonVariants } from "~/components/ui/button";
 import { useFetchMyOrders } from "~/hooks/ordering/use_fetch_my_orders";
 import { cn } from "~/utils/cn";
 
 export const Orders = () => {
+  const router = useRouter();
   const orders = useFetchMyOrders(1, 3);
 
   return (
     <div className="bg-white p-4 shadow-sm">
       <div className="flex h-full w-full flex-col">
         <div className="mb-2 flex justify-between">
-          <h2 className="text-lg font-medium tracking-tight">Orders</h2>
+          <h2 className="text-lg font-medium tracking-tight">Last Orders</h2>
         </div>
         <div className="flex flex-col gap-2">
           {orders.data?.data.map((order) => (
@@ -20,6 +24,7 @@ export const Orders = () => {
               className={cn(
                 "flex cursor-pointer items-center justify-between border p-4 shadow-sm hover:bg-gray-50",
               )}
+              onClick={() => router.push(`/account/orders/${order.id}`)}
             >
               <h3 className="mb-1 text-base font-medium tracking-tight">
                 <span className="text-xs italic">{order.id}</span>
@@ -44,6 +49,12 @@ export const Orders = () => {
               </h3>
             </div>
           ))}
+        </div>
+
+        <div className="mt-4 flex items-center justify-center">
+          <Link href="/account/orders" className={buttonVariants({})}>
+            View all orders
+          </Link>
         </div>
       </div>
     </div>
