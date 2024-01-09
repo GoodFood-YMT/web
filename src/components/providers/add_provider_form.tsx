e8456f62acbc968845bb0409d15cf4532cd5d6d7
MatthieuGallice <frooztor@gmail.com>
2024-01-09 14:28:31 +0100

    component provider: submit form

--- src/components/providers/add_provider_form.tsx
+++ src/components/providers/add_provider_form.tsx
@@ -1,26 +1,45 @@
 "use client";
 import React, { useState } from 'react';
 import { useCreateProvider } from "~/hooks/providers/use_create_providers";
+import toast from "react-hot-toast";
+import z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
+const formSchema = z.object({
+    name: z.string(),
+    restaurant_id: z.string(),
+});
+  
 const AddProviderForm = () => {
     const createProvider = useCreateProvider();
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+    });
+      
+    const handleSubmit = (payload: z.infer<typeof formSchema>) => {
+        createProvider.mutate(payload, {
+        onSuccess: () => {
+            toast.success("Provider created");
+        },
+        onError: () => {
+            toast.error("An error occurred");
+        },
+        });
     };
 
-
     return (
-        <form onSubmit={handleSubmit}>
-            <h1>Ajouter un fournisseur</h1>
-            <label htmlFor="name">Nom du fournisseur</label>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+            <h1>New Provider</h1>
+            <label htmlFor="name">Name</label>
             <input
                 type="text"
                 id="name"
                 name="name"
-                placeholder="Nom du fournisseur"
+                placeholder="Provider's Name"
             />
-            <button type="submit">Ajouter</button>
+            <button type="submit">Create</button>
         </form>
     );
 };
