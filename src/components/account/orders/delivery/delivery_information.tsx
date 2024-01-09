04bb84321d6153d071499a998cba262021567d52
tom-brulin <t.brulin@outlook.com>
2024-01-09 12:08:44 +0100

    feat: order details design

--- src/components/account/orders/delivery/delivery_information.tsx
+++ src/components/account/orders/delivery/delivery_information.tsx
@@ -12,6 +12,7 @@ interface Props {
 
 export const DeliveryInformation = ({ id }: Props) => {
   const delivery = useFetchDeliveryById(id);
+  const address = useFetchAddressById(delivery.data?.address_id);
 
   if (delivery.isLoading) {
     return <AiOutlineLoading className={cn("h-6 w-6 animate-spin")} />;
@@ -24,20 +25,26 @@ export const DeliveryInformation = ({ id }: Props) => {
   //   const address = useFetchAddressById(delivery.data.id);
 
   return (
-    <div className="bg-white p-4 shadow-sm">
+    <div className="h-full w-full bg-white p-4 shadow-sm">
       <div className="flex h-full w-full flex-col">
-        <h2 className="mb-2 text-lg font-medium tracking-tight">
-          Delivery - {delivery.data.id}
-        </h2>
-        <div className="grid grid-cols-[auto,1fr] gap-x-4 gap-y-1 text-sm">
-          <span className="font-medium">Address</span>
+        <h2 className="mb-2 flex items-center justify-between text-lg font-medium tracking-tight">
           <span>
-            {delivery.data.address_id}
-            {/* {address.data?.street}, {address.data?.city} */}
+            Delivery <span className="text-sm">({delivery.data.id})</span>
           </span>
-
-          <span className="font-medium">Status</span>
-          <span className="capitalize">{delivery.data.status}</span>
+          <span className="rounded-full bg-orange-400 px-2 py-1 text-xs capitalize text-white">
+            {delivery.data.status}
+          </span>
+        </h2>
+        <div className="grid grid-cols-[auto,1fr] gap-x-4 gap-y-2 text-sm">
+          {address.data && (
+            <>
+              <span className="font-medium">Address</span>
+              <span>
+                {address.data.street}, {address.data.zip_code}{" "}
+                {address.data.city}, {address.data.country}
+              </span>
+            </>
+          )}
         </div>
       </div>
     </div>
