c044a6aadbf09f316e8fccccc37569a0fb9c86d6
MatthieuGallice <frooztor@gmail.com>
2024-02-05 16:13:29 +0100

    feat : provider design : delete on click

--- src/components/providers/one_provider.tsx
+++ src/components/providers/one_provider.tsx
@@ -2,8 +2,10 @@
 
 import { AiOutlineLoading } from "react-icons/ai";
 import { useFetchAllProvidersIngredients } from "~/hooks/providers/use_fetch_all_ingredients_providers_by_id";
+import toast from "react-hot-toast";
 import { cn } from "~/utils/cn";
 import { AddIngredientProvider } from "~/components/providers/add_ingredient_provider";
+import { useDeleteProviderIngredient } from "~/components/providers/use_delete_ingredients_providers_by_id";
 import {
   Table,
   TableBody,
@@ -12,13 +14,40 @@ import {
   TableHeader,
   TableRow,
 } from "~/components/ui/table";
+import { Button } from "~/components/ui/button";
+import { useState } from "react"; 
 
 interface Props {
   id: string;
 }
 
 export const OneProvider = ({ id }: Props) => {
+  const [isLoading, setIsLoading] = useState(false); 
   const providers_ingredients = useFetchAllProvidersIngredients(id);
+
+  const deleteProviderIngredient = useDeleteProviderIngredient();
+  const handleDeleteRow = async (providerId: string, ingredientId: string) => {
+    try {
+      setIsLoading(true);
+  
+      await deleteProviderIngredient.mutate(
+        { provider_id: providerId, ingredient_id: ingredientId },
+        {
+          onSuccess: () => {
+            toast.success("Ingredient deleted successfully");
+          },
+          onError: () => {
+            toast.error("An error occurred while deleting the ingredient");
+          },
+        }
+      );
+      await providers_ingredients.refetch();
+    } catch (error) {
+      console.error("Erreur lors de la suppression", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   if (providers_ingredients.isError) {
     return <div>Une erreur est survenue</div>;
@@ -40,9 +69,13 @@ export const OneProvider = ({ id }: Props) => {
               <TableRow>
                 <TableCell>{providers_ingredients.ingredient[0].name}</TableCell>
                 <TableCell>
-                  <link href="">
-                    Delete
-                  </link>
+                  <Button
+                    type="button"
+                    onClick={() => handleDeleteRow(providers_ingredients.provider_id ,providers_ingredients.ingredient[0].id)}
+                    disabled={isLoading}
+                  >
+                    {isLoading ? "Deleting..." : "Delete"}
+                  </Button>
                 </TableCell>
               </TableRow>
             )),
