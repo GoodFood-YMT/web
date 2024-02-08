305baa83412eca4b3abac846c5494a9ebc2d27d8
tom-brulin <t.brulin@outlook.com>
2024-02-08 10:23:00 +0100

    fix: all ingredients w/o pagination

--- src/components/ingredients/all_ingredients.tsx
+++ src/components/ingredients/all_ingredients.tsx
@@ -16,28 +16,15 @@ export const AllIngredients = () => {
   return (
     <>
       <div className={cn("grid grid-cols-4")}>
-        {ingredients.data?.pages.map((page) =>
-          page.data.map((ingredient) => (
-            <Link href={`/ingredients/${ingredient.id}`} key={ingredient.id}>
-              {ingredient.name}
-            </Link>
-          )),
-        )}
+        {ingredients.data?.data.map((ingredient) => (
+          <Link href={`/ingredients/${ingredient.id}`} key={ingredient.id}>
+            {ingredient.name}
+          </Link>
+        ))}
 
         {ingredients.isLoading && (
           <div className="flex items-center justify-center py-8">
             <AiOutlineLoading className={cn("h-6 w-6 animate-spin")} />
-          </div>
-        )}
-
-        {ingredients.hasNextPage && (
-          <div className="mt-4 flex justify-center">
-            <Button
-              onClick={() => ingredients.fetchNextPage()}
-              disabled={ingredients.isLoading}
-            >
-              Load more
-            </Button>
           </div>
         )}
       </div>
